cfe75d7fe4b365dfa97878e78650698f6e4add63
Sangjun-man <molung@icloud.com>
2023-03-01 01:19:35 +0900

    feat : auth  status 체크 controller 추가

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller, Get, Req } from '@nestjs/common';
+import { Request } from 'express';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +9,16 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('status')
+  user(@Req() request: Request) {
+    console.log(request.user);
+
+    if (request.user) {
+      return { msg: 'Authenticated', data: request.user };
+    } else {
+      return { msg: 'notAuth' };
+    }
   }
 }
